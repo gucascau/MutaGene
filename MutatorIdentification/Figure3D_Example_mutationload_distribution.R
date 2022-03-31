e4ff1253200b623c6e72829b8c53ad6343a81f53
gucascau <xinwangcau@gmail.com>
2022-03-31 17:37:54 -0400

    Add files via upload

--- MutatorIdentification/Figure3D_Example_mutationload_distribution.R
+++ MutatorIdentification/Figure3D_Example_mutationload_distribution.R
@@ -15,7 +15,7 @@ setwd("/Users/xinwang/Documents/Projects/Proposal/Figure3/MainFigureV1020_202203
 
 #myfiles<-list.files(pattern="*associatedgenes.txt"
 
-#### show the low number of silent mutations
+#### show the low number of mutations
 
 Mutationload_gene<-read.table("Mutationload_DecMut/CNVdecreased_Omutators.forMLF2.txt",header = T,sep = "\t", na.strings = NA, fill = T)
 
@@ -23,7 +23,7 @@ attach(Mutationload_gene)
 levels(Type)
 
 Figure3DCNVdata<-cbind(Mutationload_gene,log10(Mutationload_gene$Mutationload))
-write.table(as.matrix(Figure3DCNVdata),file="Fig3DPartSNVC1_datasouce.xls",row.names = TRUE, sep="\t")
+write.table(as.matrix(Figure3DCNVdata),file="Fig3DPart2_datasouce.xls",row.names = TRUE, sep="\t")
 
 
 ggplot(Mutationload_gene,aes(x=factor(Mutationload_gene$Gene),y=log10(Mutationload_gene$Mutationload),fill=Type))+
@@ -54,6 +54,8 @@ Mutationload_gene<-read.table("Mutationload_IncMut/CNVInc_Omutators.forMLF2.txt"
 attach(Mutationload_gene)
 levels(Mutationload_gene$Type)
 
+Figure3D2CNVdata<-cbind(Mutationload_gene,log10(Mutationload_gene$Mutationload))
+write.table(as.matrix(Figure3DCNVdata),file="Fig3DPart1_datasouce.xls",row.names = TRUE, sep="\t")
 
 ggplot(Mutationload_gene,aes(x=factor(Mutationload_gene$Gene),y=log10(Mutationload_gene$Mutationload),fill=Type))+
   # geom_violin()+ 
@@ -77,12 +79,16 @@ ggplot(Mutationload_gene,aes(x=factor(Mutationload_gene$Gene),y=log10(Mutationlo
 
 ##### for lists indel gene increased
 
-setwd("/Users/xinwang/Documents/Project/Proposal/Figure3/Overlapping_Mutators_20200623/OverlappingMutator_forfigures/Example_intergrated/Indelload_IncMut")
+#setwd("/Users/xinwang/Documents/Project/Proposal/Figure3/Overlapping_Mutators_20200623/OverlappingMutator_forfigures/Example_intergrated/Indelload_IncMut")
 
-Mutationload_gene<-read.table("CNVInc_IndelloadExam2.txt",header = T,sep = "\t", na.strings = NA, fill = T)
+Mutationload_gene<-read.table("Indelload_IncMut/CNVInc_IndelloadExam2.txt",header = T,sep = "\t", na.strings = NA, fill = T)
 
 attach(Mutationload_gene)
 levels(Mutationload_gene$Type)
+
+
+Figure3D3CNVdata<-cbind(Mutationload_gene,log10(Mutationload_gene$Indelload))
+write.table(as.matrix(Figure3D3CNVdata),file="Fig3DPart3_datasouce.xls",row.names = TRUE, sep="\t")
 
 
 ggplot(Mutationload_gene,aes(x=factor(Gene),y=log10(Indelload),fill=Type))+
@@ -105,12 +111,16 @@ ggplot(Mutationload_gene,aes(x=factor(Gene),y=log10(Indelload),fill=Type))+
 
 ##### for lists indel gene decreased
 
-setwd("/Users/xinwang/Documents/Project/Proposal/Figure3/Overlapping_Mutators_20200623/OverlappingMutator_forfigures/Example_intergrated/Indelload_DecMut")
+#setwd("/Users/xinwang/Documents/Project/Proposal/Figure3/Overlapping_Mutators_20200623/OverlappingMutator_forfigures/Example_intergrated/Indelload_DecMut")
 
-  Mutationload_gene<-read.table("CNVdec_Indelloadexam2.txt",header = T,sep = "\t", na.strings = NA, fill = T)
+  Mutationload_gene<-read.table("Indelload_DecMut/CNVdec_Indelloadexam2.txt",header = T,sep = "\t", na.strings = NA, fill = T)
 
 attach(Mutationload_gene)
 levels(Mutationload_gene$Type)
+
+Figure3D4CNVdata<-cbind(Mutationload_gene,log10(Mutationload_gene$Indelload))
+write.table(as.matrix(Figure3D4CNVdata),file="Fig3DPart4_datasouce.xls",row.names = TRUE, sep="\t")
+
 
 
 ggplot(Mutationload_gene,aes(x=factor(Gene),y=log10(Indelload),fill=Type))+
@@ -134,12 +144,16 @@ ggplot(Mutationload_gene,aes(x=factor(Gene),y=log10(Indelload),fill=Type))+
 
 ##### for lists CNV gene decreased
 
-setwd("/Users/xinwang/Documents/Project/Proposal/Figure3/Overlapping_Mutators_20200623/OverlappingMutator_forfigures/Example_intergrated/CNVload_DecMut/")
+#setwd("/Users/xinwang/Documents/Project/Proposal/Figure3/Overlapping_Mutators_20200623/OverlappingMutator_forfigures/Example_intergrated/CNVload_DecMut/")
 
-  Mutationload_gene<-read.table("CNVload_decMutExam2.txt",header = T,sep = "\t", na.strings = NA, fill = T)
+  Mutationload_gene<-read.table("CNVload_DecMut/CNVload_decMutExam2.txt",header = T,sep = "\t", na.strings = NA, fill = T)
 
 attach(Mutationload_gene)
 levels(Mutationload_gene$Type)
+
+Figure3D5CNVdata<-cbind(Mutationload_gene,log10(Mutationload_gene$CNV))
+write.table(as.matrix(Figure3D5CNVdata),file="Fig3DPart5_datasouce.xls",row.names = TRUE, sep="\t")
+
 
 
 ggplot(Mutationload_gene,aes(x=factor(Gene),y=log10(CNV),fill=Type))+
@@ -163,12 +177,16 @@ ggplot(Mutationload_gene,aes(x=factor(Gene),y=log10(CNV),fill=Type))+
 
 ##### for lists CNV gene increase
 
-setwd("/Users/xinwang/Documents/Project/Proposal/Figure3/Overlapping_Mutators_20200623/OverlappingMutator_forfigures/Example_intergrated/CNVload_IncMut/")
+#setwd("/Users/xinwang/Documents/Project/Proposal/Figure3/Overlapping_Mutators_20200623/OverlappingMutator_forfigures/Example_intergrated/CNVload_IncMut/")
 
-Mutationload_gene<-read.table("CNVload_increaseMut2.txt",header = T,sep = "\t", na.strings = NA, fill = T)
+Mutationload_gene<-read.table("CNVload_IncMut/CNVload_increaseMut2.txt",header = T,sep = "\t", na.strings = NA, fill = T)
 
 attach(Mutationload_gene)
 levels(Mutationload_gene$Type)
+
+Figure3D6CNVdata<-cbind(Mutationload_gene,log10(Mutationload_gene$CNV))
+write.table(as.matrix(Figure3D6CNVdata),file="Fig3DPart6_datasouce.xls",row.names = TRUE, sep="\t")
+
 
 
 ggplot(Mutationload_gene,aes(x=factor(Gene),y=log10(CNV),fill=Type))+
